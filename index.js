d00e2b94e1c86123f98a7dfaf880f96ee3e04447
ManuhManuh <cara.sterling200@gmail.com>
2024-09-21 02:39:22 -0600

    Updated portfolio
    New portfolio page, plus individual pages for each project

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-$("button").on("click", function(event){    
-    console.log(event.target.id);
+$("img, button").on("click", function(event){   
+    animatedPress(event.target.id);
     switch(event.target.id){
         case "portfolio": window.location.href = "./pages/portfolio.html";
             break;
-        case "new-recipe": window.location.href = "newrecipe.html";
+        case "orienteering": window.location.href = "orienteering.html";
             break;
         case "octavicopter": window.location.href = "octavicopter.html";
+            break;
+        case "new-recipe": window.location.href = "newrecipe.html";
+            break;
+        case "tower-defense": window.location.href = "towerdefense.html";
+            break;
+        case "mud-run": window.location.href = "mudrun.html";
+            break;
+        case "eggs": window.location.href = "eggs.html";
+            break;
+        case "ez-button": window.location.href = "ezbutton.html";
+            break;
+        case "desk-forest": window.location.href = "deskforest.html";
+            break;
+        case "vera": window.location.href = "vera.html";
             break;
         case "back": window.location.href = "../pages/portfolio.html";
             break;
         case "home": window.location.href = "../index.html";
             break;
     }
-    //`window.location.href = "./pages/newrec.html";
 });
+
+function animatedPress(name){
+    var clickedImage = $("#"+ name);
+    clickedImage.addClass("pressed");
+    setTimeout(function() {
+        clickedImage.removeClass("pressed");
+      }, 100);
+
+}
